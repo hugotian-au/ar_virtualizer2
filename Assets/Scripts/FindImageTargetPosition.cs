2ba19678bb7f2d7009209345d3df3a5a28a88278
HugoTian <huayuantian@mymail.unisa.edu.au>
2022-02-20 16:56:58 +1030

    Instaniate the local player

--- Assets/Scripts/FindImageTargetPosition.cs
+++ Assets/Scripts/FindImageTargetPosition.cs
@@ -3,11 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.WSA;
 using Microsoft.MixedReality.Toolkit.Experimental.Utilities;
-using Photon.Pun;
-using Photon.Realtime;
 
 
-public class FindImageTargetPosition : MonoBehaviourPunCallbacks
+public class FindImageTargetPosition : MonoBehaviour
 {
     public Vector3 origin_position;
     private GameObject marker;
@@ -32,44 +30,4 @@ public class FindImageTargetPosition : MonoBehaviourPunCallbacks
             print("origin_position is: " + origin_position);
         }
     }
-
-    public override void OnPlayerEnteredRoom(Player other)
-    {
-        Debug.LogFormat("OnPlayerEnteredRoom() {0}", other.NickName); // not seen if you're the player connecting
-
-
-        if (PhotonNetwork.IsMasterClient)
-        {
-            Debug.LogFormat("OnPlayerEnteredRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient); // called before OnPlayerLeftRoom
-            // LoadArena();
-        }
-    }
-
-    public override void OnPlayerLeftRoom(Player other)
-    {
-        Debug.LogFormat("OnPlayerLeftRoom() {0}", other.NickName); // seen when other disconnects
-
-
-        if (PhotonNetwork.IsMasterClient)
-        {
-            Debug.LogFormat("OnPlayerLeftRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient); // called before OnPlayerLeftRoom
-            // LoadArena();
-        }
-    }
-
-    public void OnDestroy()
-    {
-        Debug.Log("Leave the room OnDestroy");
-        // PhotonNetwork.LeaveRoom();
-    }
-
-    #region Photon Callbacks
-    /// <summary>
-    /// Called when the local player left the room. We need to load the launcher scene.
-    /// </summary>
-    public override void OnLeftRoom()
-    {
-        print("Leave the room!");
-    }
-    #endregion
 }
