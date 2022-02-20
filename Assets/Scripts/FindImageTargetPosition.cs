c816783c127350ad64e0bd46ce574f9518527f8b
HugoTian <huayuantian@mymail.unisa.edu.au>
2022-02-20 15:21:51 +1030

    Add temp code for leaving the room but not enabled

--- Assets/Scripts/FindImageTargetPosition.cs
+++ Assets/Scripts/FindImageTargetPosition.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.WSA;
 using Microsoft.MixedReality.Toolkit.Experimental.Utilities;
-using Vuforia;
+using Photon.Pun;
+using Photon.Realtime;
 
 
-public class FindImageTargetPosition : MonoBehaviour
+public class FindImageTargetPosition : MonoBehaviourPunCallbacks
 {
     public Vector3 origin_position;
     private GameObject marker;
@@ -28,7 +29,23 @@ public class FindImageTargetPosition : MonoBehaviour
                 origin_position = pos;
                 transform.localPosition = origin_position;
             }
+            print("origin_position is: " + origin_position);
         }
-        print("origin_position is: " + origin_position);
     }
+
+    public void OnDestroy()
+    {
+        Debug.Log("Leave the room");
+        // PhotonNetwork.LeaveRoom();
+    }
+
+    #region Photon Callbacks
+    /// <summary>
+    /// Called when the local player left the room. We need to load the launcher scene.
+    /// </summary>
+    public override void OnLeftRoom()
+    {
+        print("Leave the room!");
+    }
+    #endregion
 }
