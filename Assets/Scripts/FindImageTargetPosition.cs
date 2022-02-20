3cab745031c2744d804adccb64bd5d01e4c972c8
HugoTian <huayuantian@mymail.unisa.edu.au>
2022-02-20 15:37:09 +1030

    Add code to mointor player entering/leaving room

--- Assets/Scripts/FindImageTargetPosition.cs
+++ Assets/Scripts/FindImageTargetPosition.cs
@@ -33,9 +33,33 @@ public class FindImageTargetPosition : MonoBehaviourPunCallbacks
         }
     }
 
+    public override void OnPlayerEnteredRoom(Player other)
+    {
+        Debug.LogFormat("OnPlayerEnteredRoom() {0}", other.NickName); // not seen if you're the player connecting
+
+
+        if (PhotonNetwork.IsMasterClient)
+        {
+            Debug.LogFormat("OnPlayerEnteredRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient); // called before OnPlayerLeftRoom
+            // LoadArena();
+        }
+    }
+
+    public override void OnPlayerLeftRoom(Player other)
+    {
+        Debug.LogFormat("OnPlayerLeftRoom() {0}", other.NickName); // seen when other disconnects
+
+
+        if (PhotonNetwork.IsMasterClient)
+        {
+            Debug.LogFormat("OnPlayerLeftRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient); // called before OnPlayerLeftRoom
+            // LoadArena();
+        }
+    }
+
     public void OnDestroy()
     {
-        Debug.Log("Leave the room");
+        Debug.Log("Leave the room OnDestroy");
         // PhotonNetwork.LeaveRoom();
     }
 
